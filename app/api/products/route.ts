d6deb9b396f65cf5e0e6d0ed5c21ebeb33d8c8cd
Nickszzzz <jmnicolas4me@gmail.com>
2025-01-06 23:32:30 +0800

    Update for db connection

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -9,7 +9,7 @@ export async function GET(): Promise<NextResponse> {
         const products = await prisma.product.findMany();
         return NextResponse.json({ data: products }, { status: 200 });
     } catch (error) {
-        console.error('Error:', error);
+        console.log('Error:');
         return NextResponse.json({ error: (error as Error).message }, { status: 500 });
     }
 }
@@ -20,7 +20,8 @@ export async function POST(req: Request): Promise<NextResponse> {
         const data = await req.json();
         return NextResponse.json({ data: data }, { status: 200 });
     } catch (error) {
-        console.error('Error:', error);
+        console.log('Error:');
+        console.log(error);
         return NextResponse.json({ error: (error as Error).message }, { status: 500 });
     }
 }
