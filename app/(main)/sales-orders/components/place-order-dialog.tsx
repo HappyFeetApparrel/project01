a56d77f7397d31b7c5f68481914e04afbd93ed65
Nickszzzz <jmnicolas4me@gmail.com>
2025-01-30 22:35:41 +0800

    update

--- app/(main)/sales-orders/components/place-order-dialog.tsx
+++ app/(main)/sales-orders/components/place-order-dialog.tsx
@@ -1,10 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { Plus, Minus, X, Search } from "lucide-react";
-import Image from "next/image";
-
-import { Skeleton } from "@/components/ui/skeleton";
+import { Plus, Minus, X } from "lucide-react";
 
 import { api } from "@/lib/axios";
 
@@ -42,7 +39,7 @@ import * as z from "zod";
 
 // import types
 // import { Product } from "@/data/product";
-import OrderItem from "../types/order-item";
+
 // import OrderData from "../types/order-data";
 
 const orderSchema = z.object({
@@ -59,11 +56,17 @@ interface PlaceOrderDialogProps {
 
 import { Product } from "@/prisma/type";
 
+interface OrderItem {
+  product: Product;
+  quantity_in_stock: number;
+}
+
+import ProductSearch from "./product-search";
+
 export function PlaceOrderDialog({ open, setOpen }: PlaceOrderDialogProps) {
   // const [showSuccessPopup, setShowSuccessPopup] = useState(false);
   // const [showFailPopup, setShowFailPopup] = useState(false);
   // const [showPrintInvoice, setShowPrintInvoice] = useState(false);
-  const [searchTerm, setSearchTerm] = useState("");
   const [orderItems, setOrderItems] = useState<OrderItem[]>([]);
   const [change, setChange] = useState(0);
   // const [isProcessing, setIsProcessing] = useState(false);
@@ -111,53 +114,53 @@ export function PlaceOrderDialog({ open, setOpen }: PlaceOrderDialogProps) {
   //     product.name.toLowerCase().includes(searchTerm.toLowerCase())
   // );
 
-  // const addToOrder = (product: Product) => {
-  //   const existingItem = orderItems.find(
-  //     (item) => item.product.productId === product.productId
-  //   );
-  //   if (existingItem) {
-  //     if (existingItem.quantity < product.quantity) {
-  //       setOrderItems(
-  //         orderItems.map((item) =>
-  //           item.product.productId === product.productId
-  //             ? { ...item, quantity: item.quantity + 1 }
-  //             : item
-  //         )
-  //       );
-  //     }
-  //   } else {
-  //     setOrderItems([...orderItems, { product, quantity: 1 }]);
-  //   }
-  // };
+  const addToOrder = (product: Product) => {
+    const existingItem = orderItems.find(
+      (item) => item.product.product_id === product.product_id
+    );
+    if (existingItem) {
+      if (existingItem.quantity_in_stock < product.quantity_in_stock) {
+        setOrderItems(
+          orderItems.map((item) =>
+            item.product.product_id === product.product_id
+              ? { ...item, quantity_in_stock: item.quantity_in_stock + 1 }
+              : item
+          )
+        );
+      }
+    } else {
+      setOrderItems([...orderItems, { product, quantity_in_stock: 1 }]);
+    }
+  };
 
-  const removeFromOrder = (productId: string) => {
+  const removeFromOrder = (product_id: number) => {
     setOrderItems(
-      orderItems.filter((item) => item.product.productId !== productId)
+      orderItems.filter((item) => item.product.product_id !== product_id)
     );
   };
 
-  // const updateQuantity = (productId: string, newQuantity: number) => {
-  //   const product = inventory.find((p) => p.productId === productId);
-  //   if (!product) return;
+  const updateQuantity = (product_id: number, newQuantity: number) => {
+    const product = products.find((p) => p.product_id === product_id);
+    if (!product) return;
 
-  //   if (newQuantity < 1) {
-  //     removeFromOrder(productId);
-  //   } else if (newQuantity <= product.quantity) {
-  //     setOrderItems(
-  //       orderItems.map((item) =>
-  //         item.product.productId === productId
-  //           ? { ...item, quantity: newQuantity }
-  //           : item
-  //       )
-  //     );
-  //   }
-  // };
+    if (newQuantity < 1) {
+      removeFromOrder(product_id);
+    } else if (newQuantity <= product.quantity_in_stock) {
+      setOrderItems(
+        orderItems.map((item) =>
+          item.product.product_id === product_id
+            ? { ...item, quantity_in_stock: newQuantity }
+            : item
+        )
+      );
+    }
+  };
 
   const calculateTotal = () => {
     return orderItems.reduce((total, item) => {
       const priceAfterDiscount =
-        item.product.unitPrice * (1 - item.product.discount / 100);
-      return total + priceAfterDiscount * item.quantity;
+        item.product.unit_price * (1 - (item.product.discount ?? 0) / 100);
+      return total + priceAfterDiscount * item.quantity_in_stock;
     }, 0);
   };
 
@@ -196,8 +199,8 @@ export function PlaceOrderDialog({ open, setOpen }: PlaceOrderDialogProps) {
     //   // Update inventory
     //   updateInventory(
     //     orderItems.map((item) => ({
-    //       productId: item.product.productId,
-    //       quantity: item.quantity,
+    //       product_id: item.product.product_id,
+    //       quantity_in_stock: item.quantity_in_stock,
     //     }))
     //   );
     //   setInventory(getInventory());
@@ -235,84 +238,15 @@ export function PlaceOrderDialog({ open, setOpen }: PlaceOrderDialogProps) {
           </DialogHeader>
 
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-            <div className="space-y-6">
-              {/* Product Search */}
-              <div className="relative">
-                <Search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
-                <Input
-                  placeholder="Search products by name, SKU, or barcode..."
-                  className="pl-8"
-                  value={searchTerm}
-                  onChange={(e) => setSearchTerm(e.target.value)}
-                />
-              </div>
-
-              {/* Product List */}
-              <div className="border rounded-md max-h-[50vh] overflow-y-auto">
-                {loading ? (
-                  Array.from({ length: 5 }).map((_, index) => (
-                    <div
-                      key={index}
-                      className="flex items-center gap-4 p-4 hover:bg-muted transition-colors border-b last:border-b-0 animate-pulse"
-                    >
-                      <Skeleton className="w-12 h-12 rounded-md" />
-                      <div className="flex-1">
-                        <Skeleton className="w-1/2 h-4 mb-2" />
-                        <Skeleton className="w-1/3 h-4" />
-                      </div>
-                      <div className="text-right">
-                        <Skeleton className="w-1/2 h-4 mb-2" />
-                        <Skeleton className="w-1/3 h-4" />
-                      </div>
-                      <Button size="sm" disabled>
-                        <Skeleton className="w-4 h-4" />
-                      </Button>
-                    </div>
-                  ))
-                ) : error ? (
-                  <div className="border rounded-md max-h-[50vh] overflow-y-auto">
-                    <div className="flex items-center gap-4 p-4 hover:bg-muted transition-colors border-b last:border-b-0"></div>
-                    <p className="text-red-500">{error}</p>
-                  </div>
-                ) : (
-                  products.map((product: Product, index: number) => (
-                    <div
-                      key={index}
-                      className="flex items-center gap-4 p-4 hover:bg-muted transition-colors border-b last:border-b-0"
-                    >
-                      <Image
-                        src={product.product_image ?? ""}
-                        alt={product.name}
-                        width={48}
-                        height={48}
-                        className="rounded-md object-cover w-14 h-14"
-                      />
-                      <div className="flex-1">
-                        <h4 className="font-medium">{product.name}</h4>
-                        {/* <p className="text-sm text-muted-foreground">
-                        SKU: {product.sku}
-                      </p> */}
-                      </div>
-                      <div className="text-right">
-                        <p className="font-medium">
-                          ${product.unit_price.toFixed(2)}
-                        </p>
-                        <p className="text-sm text-muted-foreground">
-                          Stock: {product.quantity_in_stock}
-                        </p>
-                      </div>
-                      <Button
-                        size="sm"
-                        // onClick={() => addToOrder(product)}
-                        disabled={product.quantity_in_stock === 0}
-                      >
-                        <Plus className="h-4 w-4" />
-                      </Button>
-                    </div>
-                  ))
-                )}
-              </div>
-            </div>
+            <ProductSearch
+              addToOrder={addToOrder}
+              products={products}
+              setProducts={setProducts}
+              loading={loading}
+              setLoading={setLoading}
+              error={error}
+              setError={setError}
+            />
 
             <div className="space-y-6">
               {/* Order Summary */}
@@ -324,7 +258,7 @@ export function PlaceOrderDialog({ open, setOpen }: PlaceOrderDialogProps) {
                   <div className="space-y-2">
                     {orderItems.map((item) => (
                       <div
-                        key={item.product.productId}
+                        key={item.product.product_id}
                         className="flex items-center justify-between"
                       >
                         <div className="flex items-center gap-2">
@@ -332,41 +266,47 @@ export function PlaceOrderDialog({ open, setOpen }: PlaceOrderDialogProps) {
                             size="icon"
                             variant="outline"
                             className="h-6 w-6"
-                            // onClick={() =>
-                            //   updateQuantity(
-                            //     item.product.productId,
-                            //     item.quantity - 1
-                            //   )
-                            // }
+                            onClick={() =>
+                              updateQuantity(
+                                item.product.product_id,
+                                item.quantity_in_stock - 1
+                              )
+                            }
                           >
                             <Minus className="h-3 w-3" />
                           </Button>
-                          <span>{item.quantity}</span>
+                          <span>{item.quantity_in_stock}</span>
                           <Button
                             size="icon"
                             variant="outline"
                             className="h-6 w-6"
-                            // onClick={() =>
-                            //   updateQuantity(
-                            //     item.product.productId,
-                            //     item.quantity + 1
-                            //   )
-                            // }
-                            disabled={item.quantity >= item.product.quantity}
+                            onClick={() =>
+                              updateQuantity(
+                                item.product.product_id,
+                                item.quantity_in_stock + 1
+                              )
+                            }
+                            disabled={
+                              item.quantity_in_stock >=
+                              item.product.quantity_in_stock
+                            }
                           >
                             <Plus className="h-3 w-3" />
                           </Button>
                         </div>
                         <span className="flex-1 mx-2">{item.product.name}</span>
                         <span>
-                          ${(item.product.unitPrice * item.quantity).toFixed(2)}
+                          $
+                          {(
+                            item.product.unit_price * item.quantity_in_stock
+                          ).toFixed(2)}
                         </span>
                         <Button
                           size="icon"
                           variant="ghost"
                           className="h-6 w-6"
                           onClick={() =>
-                            removeFromOrder(item.product.productId)
+                            removeFromOrder(item.product.product_id)
                           }
                         >
                           <X className="h-3 w-3" />
