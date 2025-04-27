ce6286bb478665b7d2812c36fca795a8c7768c60
jaymarknicolas <jmnicolas4me@gmail.com>
2025-04-27 17:18:29 +0800

    update

--- app/pdf-generator/action.ts
+++ app/pdf-generator/action.ts
@@ -1,20 +1,16 @@
 "use server";
 
-import fs from "fs/promises";
-import path from "path";
-
 export async function getLogoBytes(
   relativeLogoPath: string
 ): Promise<Uint8Array | null> {
   try {
-    const logoPath = path.join(process.cwd(), "public", relativeLogoPath);
-    console.log("logoPath");
-    console.log(logoPath);
-    console.log("logoPath");
-    const imageBytes = await fs.readFile(logoPath);
-    return imageBytes;
+    const url = `${process.env.NEXT_PUBLIC_BASE_URL}/${relativeLogoPath}`;
+    const res = await fetch(url);
+    if (!res.ok) throw new Error("Failed to fetch logo image");
+    const arrayBuffer = await res.arrayBuffer();
+    return new Uint8Array(arrayBuffer);
   } catch (error) {
-    console.error("Failed to read logo image:", error);
+    console.error("Failed to fetch logo image:", error);
     return null;
   }
 }
